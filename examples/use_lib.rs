4a3b43092c43592dc8dd7ba875f3a299049de59d
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-23 01:54:12 -0400

    fix: Update Cargo.toml edition and enhance example timing
    - Changed the Rust edition in Cargo.toml from 2024 to 2021.
    - Added timing functionality to the example in use_lib.rs to measure the duration of building the directory tree.

--- examples/use_lib.rs
+++ examples/use_lib.rs
@@ -1,7 +1,10 @@
 use du_dust::{build_directory_tree, get_largest_nodes};
 
 fn main() {
+    let start_time = std::time::Instant::now();
     let tree = build_directory_tree(vec!["/Users/hk/Dev/kunkun".to_string()], false);
+    let duration = start_time.elapsed();
+    println!("Time taken to build directory tree: {:?}", duration);
     // println!("{:#?}", tree);
     // let file = std::fs::File::create("tree.json").unwrap();
     // serde_json::to_writer(file, &tree).unwrap();
